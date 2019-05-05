eea94cf9b1637fb04615494ee4a2c1c6024806b3
Kato Takashi <taka4.kato@gmail.com>
2019-05-06 07:56:10 +0900

    14.callback関数

--- markdown_sample1.py
+++ markdown_sample1.py
@@ -10,35 +10,64 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
 html.Label('Dropdown'),
 dcc.Dropdown(
-options = [
-{'label':'佐藤', 'value':'sato'},
-{'label':'スズキ', 'value':'suzuki'},
-{'label':'田中', 'value':'tanaka'},
-],
-value = 'suzuki'
+    options=[
+        {'label': '佐藤', 'value': 'sato'},
+        {'label': '鈴木', 'value': 'suzuki'},
+        {'label': '田中', 'value': 'tanaka'},
+    ],
+    value='suzuki'
 ),
 
 html.Label('Multi-Select Dropdown'),
 dcc.Dropdown(
-options = [
-{'label':'佐藤', 'value':'sato'},
-{'label':'スズキ', 'value':'suzuki'},
-{'label':'田中', 'value':'tanaka'},
-],
-value = ['sato', 'suzuki'],
-multi = True
+    options=[
+        {'label': '佐藤', 'value': 'sato'},
+        {'label': '鈴木', 'value': 'suzuki'},
+        {'label': '田中', 'value': 'tanaka'},
+    ],
+    value=['sato','suzuki'],
+    multi=True
 ),
 
 html.Label('Radio Items'),
 dcc.RadioItems(
-options = [
-{'label':'佐藤', 'value':'sato'},
-{'label':'スズキ', 'value':'suzuki'},
-{'label':'田中', 'value':'tanaka'},
-],
-value = 'suzuki'
+    options=[
+        {'label': '佐藤', 'value': 'sato'},
+        {'label': '鈴木', 'value': 'suzuki'},
+        {'label': '田中', 'value': 'tanaka'},
+    ],
+    value='suzuki'
+),
+
+html.Label('Checkboxes'),
+dcc.Checklist(
+    options=[
+        {'label': '佐藤', 'value': 'sato'},
+        {'label': '鈴木', 'value': 'suzuki'},
+        {'label': '田中', 'value': 'tanaka'},
+    ],
+    values=['suzuki', 'tanaka']
+),
+
+html.Label('Text Input'),
+dcc.Input(value='佐藤', type='text'),
+
+html.Label('Slider'),
+dcc.Slider(
+    min=0,
+    max=10,
+    marks={
+        0: '0 °F',
+        3: '3 °F',
+        5: '5 °F',
+        7.65: '7.65 °F',
+        10: '10 °F'
+    },
+    value=5
 )
 
-])
+], style={'columnCount': 2})
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
